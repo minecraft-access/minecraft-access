f3b3ab24b69d3a2ab4fe0c9e37c0a6a21d93ae20
molitvan <vbezik104@gmail.com>
2024-11-09 02:10:29 +0100

    Remove legacy POIBlocks code

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/features/point_of_interest/POIBlocks.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/point_of_interest/POIBlocks.java
@@ -80,16 +80,6 @@ public class POIBlocks {
             .map(b -> (Predicate<BlockState>) state -> state.isOf(b))
             .toList();
 
-    private TreeMap<Double, Vec3d> oreBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> doorBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> buttonBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> ladderBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> leverBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> trapDoorBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> fluidBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> otherBlocks = new TreeMap<>();
-    private TreeMap<Double, Vec3d> markedBlocks = new TreeMap<>();
-
     private Set<BlockPos> checkedBlocks = Set.of();
     private boolean enabled;
     private boolean detectFluidBlocks;
@@ -136,16 +126,6 @@ public class POIBlocks {
             if (client.currentScreen != null) return; //Prevent running if any screen is opened
             this.player = client.player;
             this.world = client.world;
-
-            oreBlocks = new TreeMap<>();
-            doorBlocks = new TreeMap<>();
-            buttonBlocks = new TreeMap<>();
-            ladderBlocks = new TreeMap<>();
-            leverBlocks = new TreeMap<>();
-            trapDoorBlocks = new TreeMap<>();
-            fluidBlocks = new TreeMap<>();
-            otherBlocks = new TreeMap<>();
-            markedBlocks = new TreeMap<>();
 
             for (POIGroup group : builtInGroups.values()) {
                 group.clearBlocks();
@@ -199,9 +179,6 @@ public class POIBlocks {
             return;
         }
 
-        // String soundType = checkAndPutIntoMap(blockPos, blockState);
-        // playSoundAtBlock(blockPos, soundType);
-
         boolean shouldPlayMarkedOnly = onPOIMarkingNow && POIMarkingConfigMap.getInstance().isSuppressOtherWhenEnabled();
 
         for (POIGroup group : builtInGroups.values()) {
@@ -227,92 +204,6 @@ public class POIBlocks {
                 world.playSound(player, pos, markedGroup.getSound(), SoundCategory.BLOCKS, volume, markedGroup.getSoundPitch());
             }
         }
-    }
-
-    private String checkAndPutIntoMap(BlockPos blockPos, BlockState blockState) {
-        String soundType = "";
-        Block block = blockState.getBlock();
-        Vec3d blockVec3dPos = blockPos.toCenterPos();
-        Vec3d playerEyePos = this.player.getEyePos(); // post 1.17
-//        Vec3d playerVec3dPos = (new Vec3d(client.player.getX(), client.player.getEyeY(), client.player.getZ())); // pre 1.17
-
-        double diff = playerEyePos.distanceTo(blockVec3dPos);
-
-        if (markedBlock.test(blockState)) {
-            markedBlocks.put(diff, blockVec3dPos);
-            soundType = "mark";
-        } else if (this.detectFluidBlocks && block instanceof FluidBlock && PlayerUtils.isNotInFluid()) {
-            if (this.world.getFluidState(blockPos).getLevel() == 8) {
-                fluidBlocks.put(diff, blockVec3dPos);
-                soundType = "blocks";
-            }
-        } else if (oreBlockPredicates.stream().anyMatch($ -> $.test(blockState))) {
-            oreBlocks.put(diff, blockVec3dPos);
-            soundType = "ore";
-        } else if (block instanceof ButtonBlock) {
-            buttonBlocks.put(diff, blockVec3dPos);
-            soundType = "blocks";
-        } else if (block instanceof LadderBlock) {
-            ladderBlocks.put(diff, blockVec3dPos);
-            soundType = "blocks";
-        } else if (block instanceof TrapdoorBlock) {
-            trapDoorBlocks.put(diff, blockVec3dPos);
-            soundType = "blocks";
-        } else if (block instanceof LeverBlock) {
-            leverBlocks.put(diff, blockVec3dPos);
-            soundType = "blocks";
-        } else if (block instanceof DoorBlock) {
-            Set<Map.Entry<Property<?>, Comparable<?>>> entries = blockState.getEntries().entrySet();
-            for (Map.Entry<Property<?>, Comparable<?>> i : entries) {
-
-                if (i.getKey().getName().equals("half")) {
-                    if (i.getValue().toString().equals("upper")) {
-                        doorBlocks.put(diff, blockVec3dPos);
-                        soundType = "blocks";
-                    }
-                    break;
-                }
-
-            }
-        } else if (poiBlockPredicates.stream().anyMatch($ -> $.test(blockState))) {
-            otherBlocks.put(diff, blockVec3dPos);
-            soundType = "blocks";
-        } else if (blockState.createScreenHandlerFactory(this.world, blockPos) != null) {
-            otherBlocks.put(diff, blockVec3dPos);
-            soundType = "blocksWithInterface";
-        }
-        return soundType;
-    }
-
-    private void playSoundAtBlock(BlockPos blockPos, String soundType) {
-        boolean playSound = this.playSound && !soundType.isEmpty() && this.volume != 0;
-        if (!playSound) return;
-
-        String coordinates = "x:%d y:%d z%d".formatted(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-
-        if (onPOIMarkingNow) {
-            if (soundType.equalsIgnoreCase("mark")) {
-                log.debug("Play sound at [{}]", coordinates);
-                this.world.playSound(this.player, blockPos, SoundEvents.ENTITY_ITEM_PICKUP,
-                        SoundCategory.BLOCKS, volume, -5f);
-                return;
-            } else if (POIMarkingConfigMap.getInstance().isSuppressOtherWhenEnabled()) {
-                log.debug("Suppress sound at [{}]", coordinates);
-                return;
-            }
-        }
-
-        log.debug("Play sound at [{}]", coordinates);
-
-        if (soundType.equalsIgnoreCase("ore"))
-            this.world.playSound(this.player, blockPos, SoundEvents.ENTITY_ITEM_PICKUP,
-                    SoundCategory.BLOCKS, volume, -5f);
-        else if (this.playSoundForOtherBlocks && soundType.equalsIgnoreCase("blocks"))
-            this.world.playSound(this.player, blockPos, SoundEvents.BLOCK_NOTE_BLOCK_BIT.value(),
-                    SoundCategory.BLOCKS, volume, 2f);
-        else if (this.playSoundForOtherBlocks && soundType.equalsIgnoreCase("blocksWithInterface"))
-            this.world.playSound(this.player, blockPos, SoundEvents.BLOCK_NOTE_BLOCK_BANJO.value(),
-                    SoundCategory.BLOCKS, volume, 0f);
     }
 
     private void setMarkedBlock(Block block) {
