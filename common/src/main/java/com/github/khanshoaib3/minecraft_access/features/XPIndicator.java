258bda97a1aafea658c8f1d7f3f9162a40c2b0a9
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2024-11-28 11:57:52 -0600

    Properly set xp value to null instead of an arbitrary value on initialization

--- common/src/main/java/com/github/khanshoaib3/minecraft_access/features/XPIndicator.java
+++ common/src/main/java/com/github/khanshoaib3/minecraft_access/features/XPIndicator.java
@@ -5,37 +5,39 @@ import com.github.khanshoaib3.minecraft_access.utils.PlayerUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.resource.language.I18n;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * This feature speaks when the player xp level is increased or decreased.
  */
 @Slf4j
 public class XPIndicator {
-    private int previousXPLevel;
+    @Nullable
+    private Integer previousXPLevel = null;
 
     public XPIndicator() {
-        previousXPLevel = -999;
     }
 
     public void update() {
-        try {
-            MinecraftClient minecraftClient = MinecraftClient.getInstance();
-            if (minecraftClient == null) return;
-            if (minecraftClient.world == null) return;
-            if (minecraftClient.player == null) return;
-            if (minecraftClient.currentScreen != null) return;
+        MinecraftClient minecraftClient = MinecraftClient.getInstance();
+        if (minecraftClient == null) return;
+        if (minecraftClient.world == null) return;
+        if (minecraftClient.player == null) return;
+        if (minecraftClient.currentScreen != null) return;
 
-            int currentXPLevel = PlayerUtils.getExperienceLevel();
-            if (previousXPLevel == currentXPLevel) return;
-
-            boolean increased = previousXPLevel < currentXPLevel;
+        int currentXPLevel = PlayerUtils.getExperienceLevel();
+        if (previousXPLevel == null) {
             previousXPLevel = currentXPLevel;
-
-            String toSpeak = (increased) ? I18n.translate("minecraft_access.xp_indicator.increased", currentXPLevel)
-                    : I18n.translate("minecraft_access.xp_indicator.decreased", currentXPLevel);
-            MainClass.speakWithNarrator(toSpeak, true);
-        } catch (Exception e) {
-            log.error("An error occurred in xp indicator.", e);
+            return;
+        } else if (previousXPLevel == currentXPLevel) {
+            return;
         }
+
+        boolean increased = previousXPLevel < currentXPLevel;
+        previousXPLevel = currentXPLevel;
+
+        String toSpeak = (increased) ? I18n.translate("minecraft_access.xp_indicator.increased", currentXPLevel)
+                : I18n.translate("minecraft_access.xp_indicator.decreased", currentXPLevel);
+        MainClass.speakWithNarrator(toSpeak, true);
     }
 }
