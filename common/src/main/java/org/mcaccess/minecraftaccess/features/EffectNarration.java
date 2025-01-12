d982b7701205b151e1feae0fd41cb795f092cb11
molitvan <vbezik104@gmail.com>
2025-01-12 02:24:02 +0100

    Announce when losing or gaining effects

--- common/src/main/java/org/mcaccess/minecraftaccess/features/EffectNarration.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/features/EffectNarration.java
@@ -1,5 +1,9 @@
 package org.mcaccess.minecraftaccess.features;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.mcaccess.minecraftaccess.MainClass;
 import org.mcaccess.minecraftaccess.utils.KeyBindingsHandler;
 import org.mcaccess.minecraftaccess.utils.NarrationUtils;
@@ -14,6 +18,8 @@ public class EffectNarration {
     @Getter
     private static EffectNarration instance = new EffectNarration();
 
+    private List<StatusEffectInstance> previousEffects = new ArrayList<>();
+
     public void update() {
         if (MinecraftClient.getInstance() == null) return;
         if (MinecraftClient.getInstance().currentScreen != null) return;
@@ -25,12 +31,65 @@ public class EffectNarration {
                 return;
             }
 
-            String toSpeak = "";
+            StringBuilder toSpeak = new StringBuilder();
             for (StatusEffectInstance effect : MinecraftClient.getInstance().player.getStatusEffects()) {
-                toSpeak += NarrationUtils.narrateEffect(effect);
+                toSpeak.append(NarrationUtils.narrateEffect(effect));
             }
 
-            MainClass.speakWithNarrator(toSpeak, true);
+            MainClass.speakWithNarrator(toSpeak.toString(), true);
         }
+
+        List<StatusEffectInstance> currentEffects = new ArrayList<>(MinecraftClient.getInstance().player.getStatusEffects());
+
+        List<StatusEffectInstance> newEffects = new ArrayList<>();
+        List<StatusEffectInstance> lostEffects = new ArrayList<>();
+
+        for (StatusEffectInstance effect : currentEffects) {
+            boolean isNewEffect = true;
+            for (StatusEffectInstance previousEffect : previousEffects) {
+                if (effect.getEffectType().equals(previousEffect.getEffectType())) {
+                    isNewEffect = false;
+                    break;
+                }
+            }
+            if (isNewEffect) {
+                newEffects.add(effect);
+            }
+        }
+
+        for (StatusEffectInstance effect : previousEffects) {
+            boolean isLostEffect = true;
+            for (StatusEffectInstance currentEffect : currentEffects) {
+                if (effect.getEffectType().equals(currentEffect.getEffectType())) {
+                    isLostEffect = false;
+                    break;
+                }
+            }
+            if (isLostEffect) {
+                lostEffects.add(effect);
+            }
+        }
+
+        StringBuilder toSpeak = new StringBuilder();
+
+        if (!newEffects.isEmpty()) {
+            toSpeak.append(I18n.translate("minecraft_access.effect_narration.gained")).append(" ");
+            for (StatusEffectInstance effect : newEffects) {
+                toSpeak.append(I18n.translate(effect.getTranslationKey())).append(", ");
+            }
+        }
+
+        if (!lostEffects.isEmpty()) {
+            toSpeak.append(I18n.translate("minecraft_access.effect_narration.lost")).append(" ");
+            for (StatusEffectInstance effect : lostEffects) {
+                toSpeak.append(I18n.translate(effect.getTranslationKey())).append(", ");
+            }
+        }
+
+        if (toSpeak.length() > 0) {
+            MainClass.speakWithNarrator(toSpeak.toString(), true);
+        }
+
+        previousEffects = currentEffects;
     }
 }
