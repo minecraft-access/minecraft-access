106c2621503a0908dd44f643d1abef26bf80cb20
boholder <bottleholder@anche.no>
2025-01-18 22:46:37 +0800

    feat(cloth): use control + tab to switch between tab buttons

--- common/src/main/java/org/mcaccess/minecraftaccess/compat/mixin/clothconfig/ClothConfigScreenMixin.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/compat/mixin/clothconfig/ClothConfigScreenMixin.java
@@ -1,15 +1,18 @@
 package org.mcaccess.minecraftaccess.compat.mixin.clothconfig;
 
-import me.shedaniel.clothconfig2.api.AbstractConfigEntry;
 import me.shedaniel.clothconfig2.gui.AbstractTabbedConfigScreen;
 import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
 import me.shedaniel.clothconfig2.gui.ClothConfigTabButton;
+import me.shedaniel.clothconfig2.gui.entries.EmptyEntry;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,11 +31,12 @@ abstract class ClothConfigScreenMixin extends AbstractTabbedConfigScreen {
     @Final
     @Shadow
     private List<ClothConfigTabButton> tabButtons;
-    @Shadow
-    public ClothConfigScreen.ListWidget<AbstractConfigEntry<AbstractConfigEntry<?>>> listWidget;
 
     @Shadow
     public abstract Text getSelectedCategory();
+
+    @Shadow
+    public ClothConfigScreen.ListWidget<? extends Element> listWidget;
 
     @Override
     public Text getNarratedTitle() {
@@ -41,7 +45,37 @@ abstract class ClothConfigScreenMixin extends AbstractTabbedConfigScreen {
 
     @Inject(at = @At("TAIL"), method = "init")
     void init(CallbackInfo ci) {
-        tabButtons.forEach(this::addSelectableChild);
-        listWidget.children().forEach(this::addSelectableChild);
+        // There are two space entries added into listWidget in ClothConfigScreen.init()
+        // remove them since they'll affect the element selection
+        listWidget.children().removeIf(e -> e instanceof EmptyEntry);
+    }
+
+    /**
+     * Make the selected category tab button focused
+     */
+    @Override
+    public void setInitialFocus() {
+        this.setInitialFocus(tabButtons.get(this.selectedCategoryIndex));
+    }
+
+    /**
+     * Inspired by {@link net.minecraft.client.gui.widget.TabNavigationWidget#trySwitchTabsWithKey(int)}.
+     * Use Control + Tab (and Control + Shift + Tab) to switch between tab buttons.
+     */
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (Screen.hasControlDown() && keyCode == GLFW.GLFW_KEY_TAB) {
+            mca$loopSelectCategory(!Screen.hasShiftDown());
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Unique
+    private void mca$loopSelectCategory(boolean forward) {
+        int nextIndex = Math.floorMod(this.selectedCategoryIndex + (forward ? 1 : -1), this.tabButtons.size());
+        ClothConfigTabButton tabButton = this.tabButtons.get(nextIndex);
+        this.setFocused(tabButton);
+        tabButton.onPress();
     }
 }
