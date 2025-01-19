558c89b347e901fedcc97e4222880df9f65853dd
boholder <bottleholder@anche.no>
2025-01-19 20:03:02 +0800

    refactor: simplify mixin inject annotation

--- common/src/main/java/org/mcaccess/minecraftaccess/compat/mixin/clothconfig/ParentElementMixin.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/compat/mixin/clothconfig/ParentElementMixin.java
@@ -1,7 +1,6 @@
 package org.mcaccess.minecraftaccess.compat.mixin.clothconfig;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.ParentElement;
@@ -15,14 +14,13 @@ import java.util.List;
 
 @Mixin(ParentElement.class)
 public interface ParentElementMixin {
-    @SuppressWarnings("MixinExtrasUnnecessaryMutableLocal")
     @Inject(method = "computeNavigationPath(Lnet/minecraft/client/gui/navigation/GuiNavigation$Tab;)Lnet/minecraft/client/gui/navigation/GuiNavigationPath;",
             at = @At(value = "INVOKE", target = "Ljava/util/Collections;sort(Ljava/util/List;Ljava/util/Comparator;)V", shift = At.Shift.AFTER))
-    default void modifyChildrenBeforeComputeNavigationPath(CallbackInfoReturnable<GuiNavigationPath> cir, @Local LocalRef<List<? extends Element>> ref) {
+    default void modifyChildrenBeforeComputeNavigationPath(CallbackInfoReturnable<GuiNavigationPath> cir, @Local List<? extends Element> ref) {
         // Skip the tab list scroll buttons when navigating through cloth screen's widgets
         if (this instanceof ClothConfigScreen) {
             ClothConfigScreenAccessor screen = ((ClothConfigScreenAccessor) this);
-            ref.get().removeIf(e -> e == screen.getButtonLeftTab() || e == screen.getButtonRightTab());
+            ref.removeIf(e -> e == screen.getButtonLeftTab() || e == screen.getButtonRightTab());
         }
     }
 }
