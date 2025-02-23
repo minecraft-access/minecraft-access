14a1a3c5054f2c0ef64134536f48ac2b51e77491
boholder <bottleholder@anche.no>
2025-02-23 14:42:39 +0800

    feat: debug log focused component

--- common/src/main/java/org/mcaccess/minecraftaccess/mixin/ScreenMixin.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/mixin/ScreenMixin.java
@@ -1,7 +1,10 @@
 package org.mcaccess.minecraftaccess.mixin;
 
 import com.google.common.collect.ImmutableList;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ComponentPath;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.narration.NarratedElementType;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
@@ -9,9 +12,12 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.MerchantScreen;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,6 +26,9 @@ import java.util.List;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
+    @Unique
+    Logger mca$log = LoggerFactory.getLogger("org.mcaccess.minecraftaccess.mixin.ScreenMixin");
+
     @Shadow
     public abstract Component getNarrationMessage();
 
@@ -64,5 +73,12 @@ public abstract class ScreenMixin {
         }
 
         callbackInfo.cancel();
+    }
+
+    @WrapOperation(method = "keyPressed",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;changeFocus(Lnet/minecraft/client/gui/ComponentPath;)V"))
+    private void debugLogFocusedComponent(Screen instance, ComponentPath path, Operation<Void> original) {
+        mca$log.debug("Focus on path: {}", path);
+        original.call(instance, path);
     }
 }
