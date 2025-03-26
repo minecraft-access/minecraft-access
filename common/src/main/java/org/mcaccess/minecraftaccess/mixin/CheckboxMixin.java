1851552c8594dd1f814a677151083b36863b3d77
Elijah Massey <emassey0135@gmail.com>
2025-03-25 21:42:47 -0500

    Narrate whether a checkbox is checked or unchecked

---
+++ common/src/main/java/org/mcaccess/minecraftaccess/mixin/CheckboxMixin.java
@@ -0,0 +1,29 @@
+package org.mcaccess.minecraftaccess.mixin;
+
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Checkbox;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.narration.NarratedElementType;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Checkbox.class)
+public abstract class CheckboxMixin extends AbstractWidget {
+    @Shadow private boolean selected;
+
+    @Overwrite
+    public void updateWidgetNarration(NarrationElementOutput builder) {
+        if (selected) {
+            builder.add(NarratedElementType.TITLE, Component.translatable("minecraft_access.gui.checkbox_checked", this.getMessage()));
+        }
+        else {
+            builder.add(NarratedElementType.TITLE, Component.translatable("minecraft_access.gui.checkbox_unchecked", this.getMessage()));
+        }
+    }
+
+    public CheckboxMixin(int x, int y, int width, int height, Component message) {
+        super(x, y, width, height, message);
+    }
+}
