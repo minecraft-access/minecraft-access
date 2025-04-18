65b357f6b3feabd320d553a1d7a23662fed09649
Elijah Massey <emassey0135@gmail.com>
2025-04-18 16:32:44 -0500

    Do not open accessibility onboarding screen

--- common/src/main/java/org/mcaccess/minecraftaccess/mixin/MinecraftMixin.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/mixin/MinecraftMixin.java
@@ -1,11 +1,13 @@
 package org.mcaccess.minecraftaccess.mixin;
 
+import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import org.mcaccess.minecraftaccess.features.access_menu.AccessMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -22,5 +24,17 @@ public class MinecraftMixin {
         if (Screen.hasAltDown()) {
             ci.cancel();
         }
+    }
+
+    /**
+     * Minecraft brings up the accessibility onboarding screen on first
+     * launch, which speaks a message without using GameNarrator. This
+     * behaviour requires Flite on Linux, and the narrator should be enabled
+     * by default anyway because all other Minecraft Access features are
+     * enabled by default.
+     */
+    @Redirect(method = "addInitialScreens", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean redirectAdd(List<?> list, Object ScreenFactory) {
+      return false;
     }
 }
