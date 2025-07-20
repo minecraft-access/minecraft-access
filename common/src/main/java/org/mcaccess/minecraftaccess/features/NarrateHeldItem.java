bb779b6d95c208d2c71ae4cbb13c412a6b3cf4e7
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2025-07-19 20:15:11 -0500

    Refactored NarrateHeldItem

--- common/src/main/java/org/mcaccess/minecraftaccess/features/NarrateHeldItem.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/features/NarrateHeldItem.java
@@ -3,56 +3,66 @@ package org.mcaccess.minecraftaccess.features;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.JukeboxPlayable;
+import net.minecraft.world.item.JukeboxSong;
 import org.mcaccess.minecraftaccess.Config;
 import org.mcaccess.minecraftaccess.MainClass;
-import org.mcaccess.minecraftaccess.features.inventory_controls.InventoryControls;
-import org.mcaccess.minecraftaccess.mixin.GuiAccessor;
-import org.mcaccess.minecraftaccess.utils.NarrationUtils;
 
-import java.util.function.Function;
+import java.util.Optional;
 
-/**
- * This class is responsible for narrating hotbar when no inventory screen is opened.
- * For narrating hotbar when any inventory screen is opened, see {@link InventoryControls#getCurrentSlotNarrationText()}
- */
 public class NarrateHeldItem {
     private String previousItemName = "";
     private int previousItemCount = 0;
-    private int previousHotbarSlot = 0;
-    public static final Function<String, String> HOTBAR_I18N = narration -> I18n.get("minecraft_access.other.selected", narration);
-    public static final Function<String, String> EMPTY_SLOT_I18N = narration -> I18n.get("minecraft_access.inventory_controls.empty_slot", narration);
+    private int previousSelectedSlot = 0;
 
     public void tick() {
-        ItemStack currentStack = ((GuiAccessor) Minecraft.getInstance().gui).getLastToolHighlight();
-        int heldItemTooltipFade = ((GuiAccessor) Minecraft.getInstance().gui).getToolHighlightTimer();
-        boolean currentStackIsEmpty = currentStack.isEmpty();
         LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) return;
 
-        if (heldItemTooltipFade == 0 && currentStackIsEmpty && player != null) {
-            // Narrate "empty slot" when the selected slot is empty
-            narrateIfHeldChanged("", 0, player.getInventory().getSelectedSlot(), NarrateHeldItem.EMPTY_SLOT_I18N);
-        }
+        ItemStack currentItemStack = player.getMainHandItem();
+        int selectedSlot = player.getInventory().getSelectedSlot();
+        String baseItemName = getItemName(currentItemStack);
+        int itemCount = currentItemStack.getCount();
 
-        if (!currentStackIsEmpty && player != null) {
-            // Narrate held item's name and count
-            narrateIfHeldChanged(currentStack.getHoverName().getString(), currentStack.getCount(), player.getInventory().getSelectedSlot(), NarrateHeldItem.HOTBAR_I18N);
-        }
-    }
+        String itemNameWithCount = currentItemStack.isStackable() ? itemCount + " " + baseItemName : baseItemName;
 
-    private void narrateIfHeldChanged(String itemName, int itemCount, int hotbarSlot, Function<String, String> i18n) {
-        boolean nameChanged = !previousItemName.equals(itemName);
-        boolean countChanged = previousItemCount != itemCount;
-        boolean slotChanged = previousHotbarSlot != hotbarSlot;
+        boolean nameChanged = !previousItemName.equals(baseItemName);
+        boolean countChanged = itemCount != previousItemCount;
+        boolean slotChanged = selectedSlot != previousSelectedSlot;
 
         if (nameChanged || slotChanged) {
-            String itemCountText = itemCount == 0 ? "" : NarrationUtils.narrateNumber(itemCount) + " ";
-            MainClass.narrate(i18n.apply(itemCountText + itemName), true);
+            MainClass.narrate(I18n.get("minecraft_access.other.selected", itemNameWithCount), true);
         } else if (countChanged && Config.getInstance().features.narrateHeldItemsCountWhenChanged) {
             MainClass.narrate(String.valueOf(itemCount), true);
         }
-        previousItemName = itemName;
+
+        previousItemName = baseItemName;
         previousItemCount = itemCount;
-        previousHotbarSlot = hotbarSlot;
+        previousSelectedSlot = selectedSlot;
+    }
+
+    private String getItemName(ItemStack itemStack) {
+        if (itemStack.isEmpty()) {
+            return I18n.get("minecraft_access.inventory_controls.empty_slot", "");
+        }
+
+        StringBuilder itemName = new StringBuilder();
+        itemName.append(itemStack.getHoverName().getString());
+
+        Optional<JukeboxPlayable> playableOpt = Optional.ofNullable(itemStack.get(DataComponents.JUKEBOX_PLAYABLE));
+        if (playableOpt.isPresent()) {
+            Optional<ResourceKey<JukeboxSong>> songKeyOpt = playableOpt.get().song().key();
+            if (songKeyOpt.isPresent()) {
+                String songPath = songKeyOpt.get().location().getPath();
+                String translatedDiscName = I18n.get("jukebox_song.minecraft." + songPath);
+                itemName.append(" ")
+                        .append(translatedDiscName);
+            }
+        }
+
+        return itemName.toString();
     }
 }
