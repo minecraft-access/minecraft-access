a7bcc3d2117c65c1309ef1f0c7d128b0e9e81717
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2025-08-20 00:33:38 -0500

    Format Mixin files

--- common/src/main/java/org/mcaccess/minecraftaccess/mixin/MouseHandlerMixin.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/mixin/MouseHandlerMixin.java
@@ -4,11 +4,9 @@ import net.minecraft.client.MouseHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import org.mcaccess.minecraftaccess.MainClass;
-import org.mcaccess.minecraftaccess.utils.PlayerUtils;
-
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
 public class MouseHandlerMixin {
