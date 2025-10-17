f262c5eb2b0e4b1d386669dfcb0980180d0ecaf1
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2025-10-17 18:24:30 -0500

    Only initialize mod after client is set up

--- neoforge/src/main/java/org/mcaccess/minecraftaccess/neoforge/MainClassNeoForge.java
+++ neoforge/src/main/java/org/mcaccess/minecraftaccess/neoforge/MainClassNeoForge.java
@@ -2,8 +2,11 @@ package org.mcaccess.minecraftaccess.neoforge;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 import org.mcaccess.minecraftaccess.Config;
@@ -11,8 +14,13 @@ import org.mcaccess.minecraftaccess.MainClass;
 
 @Mod(value = MainClass.MOD_ID, dist = Dist.CLIENT)
 public class MainClassNeoForge {
-    public MainClassNeoForge(ModContainer container) {
-        MainClass.init();
+    public MainClassNeoForge(IEventBus modEventBus, ModContainer container) {
+        modEventBus.register(this);
         container.registerExtensionPoint(IConfigScreenFactory.class, (modContainer, parent) -> AutoConfig.getConfigScreen(Config.class, parent).get());
+    }
+
+    @SubscribeEvent
+    private void onClientSetup(final FMLClientSetupEvent event) {
+        MainClass.init();
     }
 }
