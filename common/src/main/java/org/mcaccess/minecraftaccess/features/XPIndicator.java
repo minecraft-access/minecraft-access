2a52abac3b618ed99091429eea712e19914dfebe
TheSuperGamer20578 <me@sgme.dev>
2026-01-01 17:58:19 +1100

    XPIndicator events

--- common/src/main/java/org/mcaccess/minecraftaccess/features/XPIndicator.java
+++ common/src/main/java/org/mcaccess/minecraftaccess/features/XPIndicator.java
@@ -1,26 +1,21 @@
 package org.mcaccess.minecraftaccess.features;
 
-import net.blay09.mods.balm.client.platform.event.callback.ClientLifecycleCallback;
-import net.blay09.mods.balm.client.platform.event.callback.ClientTickCallback;
 import net.blay09.mods.balm.client.platform.module.BalmClientModule;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import org.mcaccess.minecraftaccess.Config;
 import org.mcaccess.minecraftaccess.MainClass;
 import org.mcaccess.minecraftaccess.utils.NarrationUtils;
+import org.mcaccess.minecraftaccess.utils.ServerChangeDetector;
 
 /**
  * This feature narrates when the player xp level is increased or decreased.
  */
 public class XPIndicator implements BalmClientModule {
-    @Nullable
-    private Integer previousXPLevel = null;
-
     @Override
     public @NotNull Identifier getId() {
         return Identifier.fromNamespaceAndPath(MainClass.MOD_ID, "xp_indicator");
@@ -28,35 +23,20 @@ public class XPIndicator implements BalmClientModule {
 
     @Override
     public void initialize() {
-        ClientTickCallback.ClientLevelTick.AFTER.register(this::tick);
-        ClientLifecycleCallback.ConnectedToServer.EVENT.register(client -> {
-            previousXPLevel = null;
-        });
+        new ServerChangeDetector<Integer>().levelEvent(level -> {
+            assert Minecraft.getInstance().player != null;
+            return Minecraft.getInstance().player.experienceLevel;
+        }, this::onChange);
     }
 
-    private void tick(Level level) {
+    private void onChange(Level level, Integer previous, Integer value) {
         assert Minecraft.getInstance().gameMode != null;
         if (!Config.getInstance().features.xpIndicatorEnabled || !Minecraft.getInstance().gameMode.hasExperience()) {
             return;
         }
-
-        assert Minecraft.getInstance().player != null;
-        int currentXPLevel = Minecraft.getInstance().player.experienceLevel;
-        if (previousXPLevel == null) {
-            previousXPLevel = currentXPLevel;
-            return;
-        }
-        if (previousXPLevel == currentXPLevel) {
-            return;
-        }
-
-        boolean increased = previousXPLevel < currentXPLevel;
-        previousXPLevel = currentXPLevel;
-
-        String narration = I18n.get(
-                increased ? "minecraft_access.xp_indicator.increased" : "minecraft_access.xp_indicator.decreased",
-                NarrationUtils.narrateNumber(currentXPLevel)
-        );
-        MainClass.narrate(narration, true);
+        MainClass.narrate(I18n.get(
+                previous < value ? "minecraft_access.xp_indicator.increased" : "minecraft_access.xp_indicator.decreased",
+                NarrationUtils.narrateNumber(value)
+        ), true);
     }
 }
